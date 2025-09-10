96aef0effd9a32621be2d9e7754e096949baa810
Zunaid Ali <zpokean@gmail.com>
2025-09-10 12:21:54 +0600

    Setup authentication

--- crmango/webapp/urls.py
+++ crmango/webapp/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
 
     path('', views.home, name=""),
 
+    path('register', views.register, name="register"),
+
+    path('my-login', views.my_login, name="my-login"),
+
 ]
--- crmango/webapp/views.py
+++ crmango/webapp/views.py
@@ -1,7 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from django.http import HttpResponse
+from .forms import CreateUserForm, LoginForm
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+
+# Homepage
 
 def home(request):
 
     return render(request, 'webapp/index.html')
+
+# Register a user
+
+def register(request):
+
+    form = CreateUserForm()
+
+    if request.method == "POST":
+
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            
+            form.save()
+
+            return redirect("my-login")
+
+    context = {'form': form}
+
+    return render(request, 'webapp/register.html', context=context)
+
+# Login a user
+
+def my_login(request):
+
+    form = LoginForm()
+
+    if request.method == "POST":
+        
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                # return redirect("")
+    
+    context = {'form':form}
+
+    return render(request, 'webapp/my-login.html', context=context)
+
+# User logout
+
+def user_logout(request):
+
+    auth.logout(request)
+
+    return redirect("my-login")
+
